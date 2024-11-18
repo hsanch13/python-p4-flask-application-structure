ff5b63f3b2f287777a83c4ce8f8a06cde69ad67c
hsanch13 <hayleysanchez13@gmail.com>
2024-11-18 16:30:35 -0700

    response request cycle

--- server/app.py
+++ server/app.py
@@ -1,14 +1,29 @@
-from flask import Flask
+#!/usr/bin/env python3
+
+import os
+
+from flask import Flask, request, current_app, g, make_response
 
 app = Flask(__name__)
 
+@app.before_request
+def app_path():
+    g.path = os.path.abspath(os.getcwd())
+
 @app.route('/')
 def index():
-    return '<h1>Welcome to my page!</h1>'
+    host = request.headers.get('Host')
+    appname = current_app.name
+    response_body = f'''
+        <h1>The host for this page is {host}</h1>
+        <h2>The name of this application is {appname}</h2>
+        <h3>The path of this application on the user's device is {g.path}</h3>
+    '''
 
-@app.route('/<string:username>')
-def user(username):
-    return f'<h1>Profile for {username}</h1>'
+    status_code = 200
+    headers = {}
+
+    return make_response(response_body, status_code, headers)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
